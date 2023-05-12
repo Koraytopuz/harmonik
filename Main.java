7d171b0ac02c17a696b598d67a7bd2c6a2710586
Koray <91742967+Koraytopuz@users.noreply.github.com>
2023-05-12 18:36:26 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        double number,total=0;
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Bir sayı giriniz");
+        number=reader.nextDouble();
+        for(double i=1;i<=number;i++){
+            total=total+(1/i);
+        }
+        System.out.println(total);
+    }
+}
